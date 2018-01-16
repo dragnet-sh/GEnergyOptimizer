98ea5295f05d219bbfc0008262fa5d91d7a7b433
dragnet-sh <dragnet.sh@gmail.com>
2018-01-16 09:14:06 +0545

    move create room - zone to the api

--- GEnergyOptimizer/Model/Database/CoreData/CoreDataAPI.swift
+++ GEnergyOptimizer/Model/Database/CoreData/CoreDataAPI.swift
@@ -15,6 +15,8 @@ class CoreDataAPI {
         return Singleton.instance
     }
 
+    let state = GEStateController.sharedInstance
+
     lazy var persistentContainer: NSPersistentContainer = {
         let container = NSPersistentContainer(name: "GEModel")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
@@ -25,17 +27,64 @@ class CoreDataAPI {
         return container
     }()
 
+    lazy var managedContext: NSManagedObjectContext = {
+        return persistentContainer.viewContext
+    }()
+
     func getAudit(id: String) -> CDAudit? {
         Log.message(.info, message: "Core Data : Get Audit")
         let fetchRequest: NSFetchRequest<CDAudit> = CDAudit.fetchRequest()
         fetchRequest.predicate = NSPredicate(format: "identifier = %@", argumentArray: [id])
 
-        let managedContext = persistentContainer.viewContext
         guard let audit = try! managedContext.fetch(fetchRequest).first as? CDAudit else {
             Log.message(.error, message: "Guard Failed : Core Data - Audit")
             return nil
         }
 
         return audit
+    }
+
+    func createRoom(name: String, finished: @escaping ()->Void) {
+        Log.message(.info, message: "Core Data : Create Room")
+        if let identifier = state.getIdentifier() {
+            guard let cdAudit = getAudit(id: identifier) else {
+                Log.message(.error, message: "Guard Failed : CDAudit")
+                return
+            }
+
+            let room = CDRoom(context: managedContext)
+            room.name = name
+            room.createdAt = NSDate()
+            room.belongsToAudit = cdAudit
+
+            do {
+                try managedContext.save()
+            } catch let error as NSError {
+                Log.message(.error, message: error.userInfo.debugDescription)
+            }
+
+            finished()
+        }
+    }
+
+    func createZone(type: String, name: String, finished: @escaping ()->Void) {
+        Log.message(.info, message: "Core Data : Create Zone")
+        guard let cdAudit = state.getCDAudit() as? CDAudit else {
+            Log.message(.error, message: "Guard Failed : CDAudit")
+            return
+        }
+
+        let zone = CDZone(context: managedContext)
+        zone.type = type
+        zone.name = name
+        zone.belongsToAudit = cdAudit
+
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            Log.message(.error, message: error.userInfo.debugDescription)
+        }
+
+        finished()
     }
 }
