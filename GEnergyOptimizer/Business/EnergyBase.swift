16614a0ee0a56a0bd2707e323f88de5b91b936ef
dragnet-sh <dragnet.sh@gmail.com>
2018-02-15 21:02:47 +0545

    initial energy calculator base

--- GEnergyOptimizer/Business/EnergyBase.swift
+++ GEnergyOptimizer/Business/EnergyBase.swift
@@ -10,14 +10,41 @@ import Parse
 
 class EnergyCalculator {
     func compute(feature: [CDFeatureData]) {
-        let energy_star = isEnergyStar(curr_values: mapFeatureData(feature: feature)) { status in
-            if (status) {
-                return
-            }
-        }
+        Log.message(.error, message: "Please override method Compute")
+        fatalError("Must be over-ridden")
     }
 
-    func costElectricity(hour_energy_use: Double, pricing_chart: Dictionary<EPeak, Double>, peak_hour_schedule: Dictionary<EPeak, Int>) -> Double {
+    func alternateProductMatchFilter(query: PFQuery<PFObject>, curr_values: Dictionary<String, Any>) -> PFQuery<PFObject> {
+        Log.message(.error, message: "Please override method getBundleResource")
+        fatalError("Must be over-ridden")
+    }
+
+
+    func productMatchFilter(query: PFQuery<PFObject>, curr_values: Dictionary<String, Any>) -> PFQuery<PFObject> {
+        let model_number = String(describing: curr_values["Model Number"]!)
+        let company = String(describing: curr_values["Company"]!)
+
+        query.whereKey("data.company", equalTo: company)
+        query.whereKey("data.model_number", equalTo: model_number)
+
+        return query
+    }
+
+    func pricingChart() -> Dictionary<EPeak, Double> {
+        fatalError("Must be over-ridden")
+    }
+
+    func peakHourSchedule() -> Dictionary<EPeak, Int> {
+        fatalError("Must be over-ridden")
+    }
+}
+
+extension EnergyCalculator {
+
+    func costElectricity(hour_energy_use: Double) -> Double {
+        let pricing_chart = pricingChart()
+        let peak_hour_schedule = peakHourSchedule()
+
         var summer = Double(peak_hour_schedule[EPeak.summerOn]!) * hour_energy_use * Double(pricing_chart[EPeak.summerOn]!)
         summer += Double(peak_hour_schedule[EPeak.summerPart]!) * hour_energy_use * Double(pricing_chart[EPeak.summerPart]!)
         summer += Double(peak_hour_schedule[EPeak.summerOff]!) * hour_energy_use * Double(pricing_chart[EPeak.summerOff]!)
@@ -28,20 +55,8 @@ class EnergyCalculator {
         return (summer + winter)
     }
 
-    func addAlternateProductMatchFilter(query: PFQuery<PFObject>, curr_values: Dictionary<String, Any>) -> PFQuery<PFObject> {
-        Log.message(.error, message: "Please override method getBundleResource")
-        fatalError("Must be over-ridden")
-    }
+    func costGas() {
 
-
-    func addProductMatchFilter(query: PFQuery<PFObject>, curr_values: Dictionary<String, Any>) -> PFQuery<PFObject> {
-        let model_number = String(describing: curr_values["Model Number"]!)
-        let company = String(describing: curr_values["Company"]!)
-
-        query.whereKey("data.company", equalTo: company)
-        query.whereKey("data.model_number", equalTo: model_number)
-
-        return query
     }
 }
 
@@ -51,8 +66,8 @@ extension EnergyCalculator {
         Log.message(.warning, message: "Energy Star Check")
 
         if let query = PlugLoad.query() {
-
-            addProductMatchFilter(query: query, curr_values: curr_values).findObjectsInBackground { object, error in
+            let queryWithProductMatchFilter = productMatchFilter(query: query, curr_values: curr_values)
+            queryWithProductMatchFilter.findObjectsInBackground { object, error in
                 if (error == nil) {
                     Log.message(.info, message: "Parse - Plugload Query - No Error")
                 } else {
@@ -65,18 +80,16 @@ extension EnergyCalculator {
                     return
                 }
 
-                if (data.count > 0) {
-                    complete(true)
-                } else {
-                    complete(false)
-                }
+                if (data.count > 0) {complete(true)}
+                else {complete(false)}
             }
         }
     }
 
     func findBestModel(curr_values: Dictionary<String, Any>, complete: @escaping ([PlugLoad]) -> Void) {
         if let query = PlugLoad.query() {
-            addAlternateProductMatchFilter(query: query, curr_values: curr_values).findObjectsInBackground { object, error in
+            let queryWithAlternateProductMatchFilter = alternateProductMatchFilter(query: query, curr_values: curr_values)
+            queryWithAlternateProductMatchFilter.findObjectsInBackground { object, error in
                 if (error == nil) {
                     Log.message(.info, message: "Parse - Plugload Query - No Error")
                 } else {
@@ -93,26 +106,24 @@ extension EnergyCalculator {
             }
         }
     }
-
 }
 
-// *** Helper Methods ***
 extension EnergyCalculator {
 
     func mapFeatureData(feature: [CDFeatureData]) -> Dictionary<String, Any> {
-        var curr_values = Dictionary<String, Any>()
+        var mapped = Dictionary<String, Any>()
         feature.map {
             let type = InitEnumMapper.sharedInstance.enumMap[$0.type!]
             if let eBaseType = type as? BaseRowType {
                 switch eBaseType {
-                case .intRow: curr_values[$0.key!] = $0.value_int
-                case .decimalRow: curr_values[$0.key!] = $0.value_double
-                default: curr_values[$0.key!] = $0.value_string
+                case .intRow: mapped[$0.key!] = $0.value_int
+                case .decimalRow: mapped[$0.key!] = $0.value_double
+                default: mapped[$0.key!] = $0.value_string
                 }
             }
         }
 
-        return curr_values
+        return mapped
     }
 
     func getBillData(bill_type: String) -> Dictionary<EPeak, Double> {
