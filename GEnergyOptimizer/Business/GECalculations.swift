dda679e6d3fb648f6ed8d12d1ea3e0170a003b05
dragnet-sh <dragnet.sh@gmail.com>
2018-02-15 21:03:26 +0545

    entry point to the Energy Calculator

--- GEnergyOptimizer/Business/GECalculations.swift
+++ GEnergyOptimizer/Business/GECalculations.swift
@@ -20,14 +20,14 @@ public class GEnergyCalculations {
     func test() {
         Log.message(.warning, message: "******* DEBUG MESSAGE FROM XCTest !! *******")
         state.registerAuditIdentifier(auditIdentifier: "test-001")
-        compute()
+        initialize()
     }
 }
 
 
 extension GEnergyCalculations {
 
-    func compute() {
+    func initialize() {
         if let identifier = state.getIdentifier() {
             if let audit = coreDataAPI.getAudit(id: identifier) {
                 let sortDescriptor = NSSortDescriptor(key: "createdAt", ascending: true)
@@ -38,17 +38,13 @@ extension GEnergyCalculations {
                 }
 
                 for zone in results {
-                    // *** Model | Company *** //
-                    // *** Extract the Plugload Feature Data *** //
-
                     guard let feature = zone.hasFeature?.allObjects as? [CDFeatureData] else {
                         Log.message(.error, message: "Guard Failed : Feature Data - Core Data Zone")
                         return
                     }
 
                     switch GUtils.getEAppliance(rawValue: zone.type!) {
-                    case .freezerFridge: __compute__freezer__fridge(feature: feature)
-                    case .fryer: __compute__fryer(feature: feature)
+                    case .freezerFridge: getFreezerFridge().compute(feature: feature)
                     default: Log.message(.warning, message: zone.type!)
                     }
                 }
@@ -57,233 +53,7 @@ extension GEnergyCalculations {
     }
 
 
-    func __compute__freezer__fridge(feature: [CDFeatureData]) {
-
-        var curr_values = mapFeatureData(feature: feature)
-        Log.message(.warning, message: curr_values.debugDescription)
-
-        let model_number = String(describing: curr_values["Model Number"]!)
-        let company = String(describing: curr_values["Company"]!)
-
-        // **** Check to see if the device is already an Energy Star Rated **** //
-
-        let energy_star = is_energy_star(model_number: model_number, company: company) { status in
-            if (status) {
-                Log.message(.warning, message: "**** Energy Star - \(company) - \(model_number) ****")
-                return
-            }
-
-            // **** Proceed with finding the Energy Efficient Device **** //
-
-            let best_model_num = self.find_best_model_fridge_freezer(curr_values: curr_values) { freezers in
-
-                Log.message(.warning, message: freezers.debugDescription)
-
-                for freezer in freezers {
-                    var pricing_chart = self.get_bill_data(bill_type: "A-1 TOU") //ToDo: Get the Bill Type from PreAudit
-                    let peak = PeakHourCalculator()
-                    var peak_hour_schedule = peak.run(usage: "14:30 21:30,6:30 12:30") //ToDo: Get the Peak Hour Usage from PreAudit
-                    var hour_energy_use = 10.0 // ****** The final missing piece !! (Talk about this with Anthony.)
-
-                    var summer = Double(peak_hour_schedule[EPeak.summerOn]!) * hour_energy_use * Double(pricing_chart[EPeak.summerOn]!)
-                    summer += Double(peak_hour_schedule[EPeak.summerPart]!) * hour_energy_use * Double(pricing_chart[EPeak.summerPart]!)
-                    summer += Double(peak_hour_schedule[EPeak.summerOff]!) * hour_energy_use * Double(pricing_chart[EPeak.summerOff]!)
-
-                    var winter = Double(peak_hour_schedule[EPeak.winterPart]!) * hour_energy_use * Double(pricing_chart[EPeak.winterPart]!)
-                    winter += Double(peak_hour_schedule[EPeak.winterOff]!) * hour_energy_use * Double(pricing_chart[EPeak.winterOff]!)
-
-                    var total_electric = summer + winter
-                    var total_cost = total_electric
-
-                    Log.message(.warning, message: "Calculated Energy Value - \(total_cost.description)")
-                }
-            }
-        }
-    }
-
-    func __compute__fryer(feature: [CDFeatureData]) {
-
-        var curr_values = mapFeatureData(feature: feature)
-        Log.message(.warning, message: curr_values.debugDescription)
-    }
-
-    func find_best_model_fridge_freezer(curr_values: Dictionary<String, Any>, complete: @escaping ([PlugLoad])->Void) {
-
-        let prod_type = String(describing: curr_values["Product Type"]!)
-        let total_volume = String(describing: curr_values["Total Volume"]!)
-        let type = "solid_door_freezers_retrofits"
-
-        if let query = PlugLoad.query() {
-
-            query.whereKey("data.style_type", equalTo: prod_type)
-            query.whereKey("data.total_volume", equalTo: Double(total_volume))
-            query.findObjectsInBackground { object, error in
-                if (error == nil) {
-                    Log.message(.info, message: "Parse - Plugload Query - No Error")
-                } else {
-                    Log.message(.error, message: error.debugDescription)
-                    return
-                }
-
-                guard let data = object as? [PlugLoad] else {
-                    Log.message(.error, message: "Guard Failed : Plugload Data - Core Data Zone")
-                    return
-                }
-
-                complete(data)
-            }
-        }
-
-
-    }
-}
-
-// *** Helper Methods *** //
-extension GEnergyCalculations {
-
-    func mapFeatureData(feature: [CDFeatureData]) -> Dictionary<String, Any> {
-        var curr_values = Dictionary<String, Any>()
-        feature.map {
-            let type = InitEnumMapper.sharedInstance.enumMap[$0.type!]
-            if let eBaseType = type as? BaseRowType {
-                switch eBaseType {
-                case .intRow: curr_values[$0.key!] = $0.value_int
-                case .decimalRow: curr_values[$0.key!] = $0.value_double
-                default: curr_values[$0.key!] = $0.value_string
-                }
-            }
-        }
-
-        return curr_values
-    }
-
-    func is_energy_star(model_number: String, company: String, complete: @escaping (Bool)->Void) {
-        Log.message(.warning, message: "Energy Star Check")
-
-        if let query = PlugLoad.query() {
-
-            query.whereKey("data.company", equalTo: company)
-            query.whereKey("data.model_number", equalTo: model_number)
-
-            query.findObjectsInBackground { object, error in
-                if (error == nil) {
-                    Log.message(.info, message: "Parse - Plugload Query - No Error")
-                } else {
-                    Log.message(.error, message: error.debugDescription)
-                    return
-                }
-
-                guard let data = object as? [PlugLoad] else {
-                    Log.message(.error, message: "Guard Failed : Plugload Data - Core Data Zone")
-                    return
-                }
-
-                if (data.count > 0) {
-                    complete(true)
-                } else {
-                    complete(false)
-                }
-            }
-        }
-    }
-
-    func get_bill_data(bill_type: String) -> Dictionary<EPeak, Double> {
-        let rows = open_csv(filename: "pge_electric")!
-        var outgoing = Dictionary<EPeak, Double>()
-        for row in rows {
-            if row["rate"]! == bill_type {
-                let key = "\(row["season"]!)-\(row["ec_period"]!)"
-                outgoing[GUtils.getEPeak(rawValue: key)] = Double(row["energy_charge"]!)
-            }
-        }
-
-        return outgoing
-    }
-
-    func open_csv(filename: String) -> Array<Dictionary<String, String>>! {
-        let url = Bundle.main.url(forResource: filename, withExtension: "csv")!
-        let data = try! String(contentsOf: url)
-        let csv = CSwiftV(with: data)
-
-        return csv.keyedRows!
-    }
-}
-
-
-class PeakHourCalculator {
-
-    let dateFormatter = DateFormatter()
-    var outgoing = Dictionary<EPeak, Int>()
-
-    init() {
-        dateFormatter.dateFormat = "HH:mm"
-        EPeak.getAll.map { outgoing[$0] = 0 }
-    }
-
-    fileprivate func getTime(time: String) -> Date {
-        return dateFormatter.date(from: time)!
-    }
-
-    fileprivate func inBetween(now: Date, start: Date, end: Date) -> Bool {
-        let a = now.timeIntervalSince1970
-        let b = start.timeIntervalSince1970
-        let c = end.timeIntervalSince1970
-
-        return (a >= b && a < c) ? true : false
-    }
-
-    fileprivate func isSummerPeak(now: Date) -> Bool {
-        return inBetween(now: now, start: getTime(time: "12:00"), end: getTime(time: "18:00"))
-    }
-
-    fileprivate func isSummerPartialPeak(now: Date) -> Bool {
-        return inBetween(now: now, start: getTime(time: "8:30"), end: getTime(time: "12:00")) ||
-                inBetween(now: now, start: getTime(time: "18:00"), end: getTime(time: "21:30"))
-    }
-
-    fileprivate func isSummerOffPeak(now: Date) -> Bool {
-        return inBetween(now: now, start: getTime(time: "21:30"), end: getTime(time: "23:59")) ||
-                inBetween(now: now, start: getTime(time: "00:00"), end: getTime(time: "8:30"))
-    }
-
-    fileprivate func isWinterPartialPeak(now: Date) -> Bool {
-        return inBetween(now: now, start: getTime(time: "8:30"), end: getTime(time: "9:30"))
-    }
-
-    fileprivate func isWinterOffPeak(now: Date) -> Bool {
-        return inBetween(now: now, start: getTime(time: "21:00"), end: getTime(time: "23:59")) ||
-                inBetween(now: now, start: getTime(time: "00:00"), end: getTime(time: "8:30"))
-    }
-
-    public func run(usage: String) -> Dictionary<EPeak, Int> {
-
-//        let usage = "14:30 21:30,6:30 12:30"
-        let usageSplit = usage.split(separator: ",")
-
-        usageSplit.map {
-            let timeRange = $0.split(separator: " ")
-            let start = dateFormatter.date(from: String(timeRange[0]))!
-            let end = dateFormatter.date(from: String(timeRange[1]))!
-            let delta = 1
-
-            let calendar = Calendar.autoupdatingCurrent
-            var step = DateComponents()
-            step.minute = delta
-
-            var _date = start
-            while _date < end {
-
-                if isSummerOffPeak(now: _date) {outgoing[EPeak.summerOff]! += delta}
-                if isSummerPartialPeak(now: _date) {outgoing[EPeak.summerPart]! += delta}
-                if isSummerPeak(now: _date) {outgoing[EPeak.summerOn]! += delta}
-
-                if isWinterOffPeak(now: _date) {outgoing[EPeak.winterOff]! += delta}
-                if isWinterPartialPeak(now: _date) {outgoing[EPeak.winterPart]! += delta}
-
-                _date = calendar.date(byAdding: step, to: _date)!
-            }
-        }
-
-        return outgoing
+    func getFreezerFridge() -> EnergyCalculator {
+        return Refrigerator()
     }
 }
