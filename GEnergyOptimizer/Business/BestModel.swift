7b8b82e033e62f29e28355033fb42704a4a3282d
dragnet-sh <dragnet.sh@gmail.com>
2018-02-22 18:38:27 +0545

    create seperate class for Best Model

---
+++ GEnergyOptimizer/Business/BestModel.swift
@@ -0,0 +1,35 @@
+//
+// Created by Binay Budhthoki on 2/22/18.
+// Copyright (c) 2018 GeminiEnergyServices. All rights reserved.
+//
+
+import Foundation
+import Parse
+import CleanroomLogger
+
+class BestModel {
+
+    var filter: PFQuery<PFObject>
+
+    init(query: PFQuery<PFObject>) {
+        self.filter = query
+    }
+
+    func find(curr_values: Dictionary<String, Any>, complete: @escaping ([PlugLoad]) -> Void) {
+        filter.findObjectsInBackground { object, error in
+            if (error == nil) {
+                Log.message(.info, message: "Parse - Plugload Query - No Error")
+            } else {
+                Log.message(.error, message: error.debugDescription)
+                return
+            }
+
+            guard let data = object as? [PlugLoad] else {
+                Log.message(.error, message: "Guard Failed : Plugload Data - Core Data Zone")
+                return
+            }
+
+            complete(data)
+        }
+    }
+}
