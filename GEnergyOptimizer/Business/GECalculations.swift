226e70df4de2de8a8024f3f1c761bd0e9718cce9
dragnet-sh <dragnet.sh@gmail.com>
2018-02-26 19:27:55 +0545

    implement HVAC calculations

--- GEnergyOptimizer/Business/GECalculations.swift
+++ GEnergyOptimizer/Business/GECalculations.swift
@@ -26,15 +26,57 @@ extension GEnergyCalculations {
         let preAudit = try! getPreAudit()
         let zones = try! getZone()
 
-        zones.forEach { zone in
-            if let featureData = zone.hasFeature?.allObjects as? [CDFeatureData] {
-                //ToDo: Use HashMap to get rid of the Switch
-                switch GUtils.getEAppliance(rawValue: zone.type!) {
+        // **** Plugload **** //
+        getPlugload(zones: zones).forEach {
+            if let featureData = $0.hasFeature?.allObjects as? [CDFeatureData] {
+                switch applianceType(zone: $0) {
                 case .freezerFridge: Refrigerator(feature: featureData, preAudit: preAudit).compute()
-                default: Log.message(.warning, message: zone.type!)
+                default: Log.message(.warning, message: $0.type!)
                 }
             }
         }
+
+        // **** HVAC **** //
+        let hvac = getHVACFeature()
+        HVAC(feature: getHVACFeature(), preAudit: preAudit).compute()
+
+        // **** Lighting **** //
+
+    }
+
+
+    func getHVACFeature() -> [CDFeatureData] {
+
+        let model = BuilderHelper.decodeJSON(bundleResource: "preaudit")!
+        let sId2Ele = BuilderHelper.mapSectionIdsToElements(model: model)!
+        let sId2Name = BuilderHelper.mapSectionIdsToName(model: model)!
+
+        var hvacSId = Array<String>()
+        for (sectionId, sectionName) in sId2Name {
+            if sectionName.starts(with: "HVAC") {
+                hvacSId.append(sectionId)}
+        }
+
+        var gElements = Array<GElements>()
+        hvacSId.forEach {gElements.append(contentsOf: sId2Ele[$0]!)}
+
+        let hvacEId = gElements.map {$0.elementId!}
+        let preaudit = try! getPreAudit()
+
+        let feature = preaudit.filter { feature in
+            return hvacEId.contains(feature.formId!)
+        }
+
+        return feature
+    }
+
+
+    func getPlugload(zones: [CDZone]) -> [CDZone] {
+        return zones.filter { applianceType(zone: $0) != .none }
+    }
+
+    func applianceType(zone: CDZone) -> EApplianceType {
+        return GUtils.getEAppliance(rawValue: zone.type!)
     }
 
     func getZone() throws -> [CDZone] {
