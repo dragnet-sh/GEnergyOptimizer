1b9b3c8b71ae1865e518491e81ca99b61a837e9f
dragnet-sh <dragnet.sh@gmail.com>
2018-03-04 00:24:31 +0545

    hvac implemantation - outgoing rows

--- GEnergyOptimizer/Business/Device/HVAC.swift
+++ GEnergyOptimizer/Business/Device/HVAC.swift
@@ -7,19 +7,31 @@ import Foundation
 import CleanroomLogger
 
 class HVAC: EnergyBase, Computable {
-    func compute() -> [[String: String]]? {
+    func compute(complete: @escaping ([[String: String]]?) -> Void) {
         let feature = super.mappedFeature
         let preaudit = super.preAudit
 
         Log.message(.warning, message: feature.debugDescription)
         if let btuPerHour = feature["Cooling Capacity (Btu/hr)"] as? Int64, let seer = feature["SEER"] as? Int64 {
-            let annualOperationHours = 8760.00
+            let annualOperationHours: Double = PeakHourMapper().annualOperatingHours(super.operatingHours)
             let power = Double(btuPerHour / seer) / 1000
             let energy: Double = power * annualOperationHours
 
             Log.message(.warning, message: "Calculated Energy Value [HVAC] - \(energy.description)")
-        }
 
+            var entry = [String: String]()
+            entry["__annual_operation_hours"] = annualOperationHours.description
+            entry["btu_per_hour"] = btuPerHour.description
+            entry["seer"] = seer.description
+            entry["__power"] = power.description
+            entry["__energy"] = energy.description
+
+            super.outgoing.append(entry)
+            complete(super.outgoing)
+        }
+    }
+
+    func fields() -> [String]? {
         return nil
     }
 }
