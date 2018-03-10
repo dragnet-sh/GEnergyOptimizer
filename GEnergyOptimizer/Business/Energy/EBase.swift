178e767297b538ad80ccd6fcb094ca3ca0fa7f6c
dragnet-sh <dragnet.sh@gmail.com>
2018-03-10 19:43:33 +0545

    improve energy base - pull up compute - major changes

--- GEnergyOptimizer/Business/Energy/EBase.swift
+++ GEnergyOptimizer/Business/Energy/EBase.swift
@@ -37,10 +37,15 @@ class EnergyBase {
     static func createEntry(_ object: Computable, _ feature: [String: Any]) -> [String: String] {
         var entry = [String: String]()
         if let fields = object.fields() {
-            fields.filter { !$0.starts(with: "__") }
+            fields.filter {
+                        !$0.starts(with: "__")
+                    }
                     .forEach { field in
-                        if let value = feature[field] { entry[field] = String(describing: value) }
-                        else { entry[field] = "" }
+                        if let value = feature[field] {
+                            entry[field] = String(describing: value)
+                        } else {
+                            entry[field] = ""
+                        }
                     }
         }
         return entry
@@ -54,7 +59,9 @@ class EnergyBase {
 
     func bestModel(complete: @escaping (Result<[PlugLoad]>) -> Void) {
         if let query = filterQuery() {
-            BestModel(query: query).query {result in complete(result)}
+            BestModel(query: query).query { result in
+                complete(result)
+            }
             return
         }
         complete(.Error("Filter Query Not Set"))
@@ -63,6 +70,73 @@ class EnergyBase {
     open func filterQuery() -> PFQuery<PFObject>? {
         fatalError("Must be over-ridden")
     }
+
 }
 
+extension EnergyBase {
+    func electricCost() -> EnergyCost {
+        return ElectricCost(rateStructure: utilityRate(), operatingHours: operatingHours)
+    }
+
+    func gasCost() -> EnergyCost {
+        return GasCost()
+    }
+
+    func annualOperatingHours() -> Double {
+        return PeakHourMapper().annualOperatingHours(operatingHours)
+    }
+
+    func dailyOperatingHours() -> Double {
+        return PeakHourMapper().dailyOperatingHours(operatingHours)
+    }
+}
+
+extension EnergyBase {
+    typealias FeatureDataEntry = (Dictionary<String, Any>) -> [String: String]?
+
+    func __compute(delegate: Computable, type: EApplianceType,
+                   handler: @escaping FeatureDataEntry, completed: @escaping (OutgoingRows?) -> Void) {
+
+        starValidator { status, error in
+            if error == .none {
+                self.bestModel { result in
+                    switch result {
+                    case .Success(let data):
+                        data.forEach { appliance in
+
+                            // *** 1. Calculate Energy Consumption
+                            // *** 2. Calculate Cost
+                            // *** 3. Append the computed value to entry
+
+                            if let _entry = handler(appliance.data) {
+                                Log.message(.info, message: _entry.debugDescription)
+                                self.outgoing.append(_entry)
+                            } else {
+                                Log.message(.error, message: "Entry Row Nil")
+                                completed(nil)
+                                return
+                            }
+                        }
+
+                        let entity = EApplianceType.getFileName(type: .rackOven)
+                        print(self.outgoing.debugDescription)
+
+                        if let header = delegate.fields() {
+                            let rows = OutgoingRows(rows: self.outgoing, entity: entity)
+                            rows.setHeader(header: header)
+                            completed(rows)
+                        } else {completed(nil)}
+
+                    case .Error(let message):
+                        Log.message(.error, message: message)
+                        completed(nil)
+                    }
+                }
+            } else {
+                Log.message(.error, message: "\(EApplianceType.rackOven.rawValue) : Star Validator Returned Nil")
+                completed(nil)
+            }
+        }
+    }
+}
 
