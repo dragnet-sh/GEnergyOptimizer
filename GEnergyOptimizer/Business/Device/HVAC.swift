1467380a1084e22f97f312ee4873f8e53b3b058e
dragnet-sh <dragnet.sh@gmail.com>
2018-03-10 20:48:27 +0545

    refactor hvac

--- GEnergyOptimizer/Business/Device/HVAC.swift
+++ GEnergyOptimizer/Business/Device/HVAC.swift
@@ -14,33 +14,35 @@ class HVAC: EnergyBase, Computable {
     }
 
     func compute(_ complete: @escaping (OutgoingRows?) -> Void) {
+        Log.message(.info, message: "### Computing - HVAC ###")
         let feature = super.mappedFeature
-        let preaudit = super.preAudit
-
         Log.message(.warning, message: feature.debugDescription)
-        if let btuPerHour = feature["Cooling Capacity (Btu/hr)"] as? Int64, let seer = feature["SEER"] as? Int64 {
-            let annualOperationHours: Double = PeakHourMapper().annualOperatingHours(super.operatingHours)
-            let power = Double(btuPerHour / seer) / 1000
-            let energy: Double = power * annualOperationHours
 
-            Log.message(.warning, message: "Calculated Energy Value [HVAC] - \(energy.description)")
+        guard   let btuPerHour = feature["Cooling Capacity (Btu/hr)"] as? Int64,
+                let seer = feature["SEER"] as? Int64 else {
 
-            var entry = EnergyBase.createEntry(self, feature)
-            entry["__annual_operation_hours"] = annualOperationHours.description
-            entry["__power"] = power.description
-            entry["__energy"] = energy.description
-
-            super.outgoing.append(entry)
-
-            let entity = EZone.hvac.rawValue
-            let result = OutgoingRows(rows: super.outgoing, entity: entity)
-            result.setHeader(header: fields()!)
-            complete(result)
-        } else {
-            Log.message(.error, message: "HVAC - One of the Feature Data returned Nil.")
+            Log.message(.error, message: "Cooling Capacity | SEER Nil")
             complete(nil)
+            return
         }
 
+        let annualOperationHours = super.annualOperatingHours()
+        let power = Double(btuPerHour / seer) / 1000
+        let energy = power * annualOperationHours
+
+        Log.message(.info, message: "Calculated Energy Value [HVAC] - \(energy.description)")
+
+        var entry = EnergyBase.createEntry(self, feature)
+        entry["__annual_operation_hours"] = annualOperationHours.description
+        entry["__power"] = power.description
+        entry["__energy"] = energy.description
+
+        super.outgoing.append(entry)
+
+        let entity = EZone.hvac.rawValue
+        let result = OutgoingRows(rows: super.outgoing, entity: entity)
+        result.setHeader(header: fields()!)
+        complete(result)
     }
 
     func fields() -> [String]? {
